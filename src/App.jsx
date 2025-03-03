b51f8f32e792ed3eecd14a84a51dec01dfcb136b
Mark Thiessen <mark.thiessen@junglescout.com>
2025-03-03 15:36:44 -0700

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,8 @@ function App() {
           <img src={logo} className="logo react" alt="Wingin it" />
         </a>
       </div>
-      <h1>Is Ryk Healed?</h1>
+      <h1>Is Ryk Better?</h1>
       <div className="card">
-        {/* progress bar based on date from Mar 19th to 6 weeks after */}
         <div className="progress">
           <div
             className="progress-bar"
